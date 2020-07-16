f13fbf5db9112d48b78d9a360dce889d2f175008
Leslie Thompson <68393359+lesliethompson0@users.noreply.github.com>
2020-07-16 17:35:33 -0300

    Add files via upload

---
+++ JogoDaVelha/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace JogoDaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLinha1Coluna1 = new System.Windows.Forms.Button();
+            this.btnLinha1Coluna2 = new System.Windows.Forms.Button();
+            this.btnLinha1Coluna3 = new System.Windows.Forms.Button();
+            this.btnLinha2Coluna1 = new System.Windows.Forms.Button();
+            this.btnLinha2Coluna2 = new System.Windows.Forms.Button();
+            this.btnLinha2Coluna3 = new System.Windows.Forms.Button();
+            this.btnLinha3Coluna1 = new System.Windows.Forms.Button();
+            this.btnLinha3Coluna2 = new System.Windows.Forms.Button();
+            this.btnLinha3Coluna3 = new System.Windows.Forms.Button();
+            this.lblJogador = new System.Windows.Forms.Label();
+            this.lblTagline = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLinha1Coluna1
+            // 
+            this.btnLinha1Coluna1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLinha1Coluna1.Location = new System.Drawing.Point(96, 137);
+            this.btnLinha1Coluna1.Name = "btnLinha1Coluna1";
+            this.btnLinha1Coluna1.Size = new System.Drawing.Size(70, 70);
+            this.btnLinha1Coluna1.TabIndex = 0;
+            this.btnLinha1Coluna1.UseVisualStyleBackColor = true;
+            this.btnLinha1Coluna1.Click += new System.EventHandler(this.btnLinha1Coluna1_Click);
+            // 
+            // btnLinha1Coluna2
+            // 
+            this.btnLinha1Coluna2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLinha1Coluna2.Location = new System.Drawing.Point(172, 137);
+            this.btnLinha1Coluna2.Name = "btnLinha1Coluna2";
+            this.btnLinha1Coluna2.Size = new System.Drawing.Size(70, 70);
+            this.btnLinha1Coluna2.TabIndex = 1;
+            this.btnLinha1Coluna2.UseVisualStyleBackColor = true;
+            this.btnLinha1Coluna2.Click += new System.EventHandler(this.btnLinha1Coluna2_Click);
+            // 
+            // btnLinha1Coluna3
+            // 
+            this.btnLinha1Coluna3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLinha1Coluna3.Location = new System.Drawing.Point(248, 137);
+            this.btnLinha1Coluna3.Name = "btnLinha1Coluna3";
+            this.btnLinha1Coluna3.Size = new System.Drawing.Size(70, 70);
+            this.btnLinha1Coluna3.TabIndex = 2;
+            this.btnLinha1Coluna3.UseVisualStyleBackColor = true;
+            this.btnLinha1Coluna3.Click += new System.EventHandler(this.btnLinha1Coluna3_Click);
+            // 
+            // btnLinha2Coluna1
+            // 
+            this.btnLinha2Coluna1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLinha2Coluna1.Location = new System.Drawing.Point(96, 213);
+            this.btnLinha2Coluna1.Name = "btnLinha2Coluna1";
+            this.btnLinha2Coluna1.Size = new System.Drawing.Size(70, 70);
+            this.btnLinha2Coluna1.TabIndex = 3;
+            this.btnLinha2Coluna1.UseVisualStyleBackColor = true;
+            this.btnLinha2Coluna1.Click += new System.EventHandler(this.btnLinha2Coluna1_Click);
+            // 
+            // btnLinha2Coluna2
+            // 
+            this.btnLinha2Coluna2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLinha2Coluna2.Location = new System.Drawing.Point(172, 213);
+            this.btnLinha2Coluna2.Name = "btnLinha2Coluna2";
+            this.btnLinha2Coluna2.Size = new System.Drawing.Size(70, 70);
+            this.btnLinha2Coluna2.TabIndex = 4;
+            this.btnLinha2Coluna2.UseVisualStyleBackColor = true;
+            this.btnLinha2Coluna2.Click += new System.EventHandler(this.btnLinha2Coluna2_Click);
+            // 
+            // btnLinha2Coluna3
+            // 
+            this.btnLinha2Coluna3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLinha2Coluna3.Location = new System.Drawing.Point(248, 213);
+            this.btnLinha2Coluna3.Name = "btnLinha2Coluna3";
+            this.btnLinha2Coluna3.Size = new System.Drawing.Size(70, 70);
+            this.btnLinha2Coluna3.TabIndex = 5;
+            this.btnLinha2Coluna3.UseVisualStyleBackColor = true;
+            this.btnLinha2Coluna3.Click += new System.EventHandler(this.btnLinha2Coluna3_Click);
+            // 
+            // btnLinha3Coluna1
+            // 
+            this.btnLinha3Coluna1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLinha3Coluna1.Location = new System.Drawing.Point(96, 289);
+            this.btnLinha3Coluna1.Name = "btnLinha3Coluna1";
+            this.btnLinha3Coluna1.Size = new System.Drawing.Size(70, 70);
+            this.btnLinha3Coluna1.TabIndex = 6;
+            this.btnLinha3Coluna1.UseVisualStyleBackColor = true;
+            this.btnLinha3Coluna1.Click += new System.EventHandler(this.btnLinha3Coluna1_Click);
+            // 
+            // btnLinha3Coluna2
+            // 
+            this.btnLinha3Coluna2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLinha3Coluna2.Location = new System.Drawing.Point(172, 289);
+            this.btnLinha3Coluna2.Name = "btnLinha3Coluna2";
+            this.btnLinha3Coluna2.Size = new System.Drawing.Size(70, 70);
+            this.btnLinha3Coluna2.TabIndex = 7;
+            this.btnLinha3Coluna2.UseVisualStyleBackColor = true;
+            this.btnLinha3Coluna2.Click += new System.EventHandler(this.btnLinha3Coluna2_Click);
+            // 
+            // btnLinha3Coluna3
+            // 
+            this.btnLinha3Coluna3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLinha3Coluna3.Location = new System.Drawing.Point(248, 289);
+            this.btnLinha3Coluna3.Name = "btnLinha3Coluna3";
+            this.btnLinha3Coluna3.Size = new System.Drawing.Size(70, 70);
+            this.btnLinha3Coluna3.TabIndex = 8;
+            this.btnLinha3Coluna3.UseVisualStyleBackColor = true;
+            this.btnLinha3Coluna3.Click += new System.EventHandler(this.btnLinha3Coluna3_Click);
+            // 
+            // lblJogador
+            // 
+            this.lblJogador.AutoSize = true;
+            this.lblJogador.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJogador.Location = new System.Drawing.Point(143, 85);
+            this.lblJogador.Name = "lblJogador";
+            this.lblJogador.Size = new System.Drawing.Size(125, 29);
+            this.lblJogador.TabIndex = 9;
+            this.lblJogador.Text = "Jogador 1";
+            // 
+            // lblTagline
+            // 
+            this.lblTagline.AutoSize = true;
+            this.lblTagline.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblTagline.Location = new System.Drawing.Point(167, 68);
+            this.lblTagline.Name = "lblTagline";
+            this.lblTagline.Size = new System.Drawing.Size(75, 17);
+            this.lblTagline.TabIndex = 10;
+            this.lblTagline.Text = "É a vez do";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.LightGreen;
+            this.btnRestart.Location = new System.Drawing.Point(132, 379);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(146, 41);
+            this.btnRestart.TabIndex = 11;
+            this.btnRestart.Text = "Reiniciar partida";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(411, 432);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.lblTagline);
+            this.Controls.Add(this.lblJogador);
+            this.Controls.Add(this.btnLinha3Coluna3);
+            this.Controls.Add(this.btnLinha3Coluna2);
+            this.Controls.Add(this.btnLinha3Coluna1);
+            this.Controls.Add(this.btnLinha2Coluna3);
+            this.Controls.Add(this.btnLinha2Coluna2);
+            this.Controls.Add(this.btnLinha2Coluna1);
+            this.Controls.Add(this.btnLinha1Coluna3);
+            this.Controls.Add(this.btnLinha1Coluna2);
+            this.Controls.Add(this.btnLinha1Coluna1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Jogo da velha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLinha1Coluna1;
+        private System.Windows.Forms.Button btnLinha1Coluna2;
+        private System.Windows.Forms.Button btnLinha1Coluna3;
+        private System.Windows.Forms.Button btnLinha2Coluna1;
+        private System.Windows.Forms.Button btnLinha2Coluna2;
+        private System.Windows.Forms.Button btnLinha2Coluna3;
+        private System.Windows.Forms.Button btnLinha3Coluna1;
+        private System.Windows.Forms.Button btnLinha3Coluna2;
+        private System.Windows.Forms.Button btnLinha3Coluna3;
+        private System.Windows.Forms.Label lblJogador;
+        private System.Windows.Forms.Label lblTagline;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
---
+++ JogoDaVelha/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaVelha
+{
+    public partial class Form1 : Form
+    {
+        string simbolo = "X";
+        string[,] dados = new string[3, 3];
+        public Form1()
+        {
+            InitializeComponent();
+            //    MessageBox.Show("Bem Vindo ao nosso jogo da velha");
+        }
+
+        private void btnLinha1Coluna1_Click(object sender, EventArgs e)
+        {
+            //só entra no botão se o botão estiver vazio
+            if (btnLinha1Coluna1.Text == "")
+            {
+
+                //PREENCHE O SÍMBOLO
+                btnLinha1Coluna1.Text = simbolo;
+
+                //DEFINIR A POSIÇÃO DO SÍMBOLO NA MATRIZ
+                dados[0, 0] = simbolo;
+
+                //identificar vencedor (invocar vencedor)
+                IdentificarVencedor();
+
+                //preparar para o próximo jogador
+                MudarJogador();
+            }
+        }
+
+        private void btnLinha1Coluna2_Click(object sender, EventArgs e)
+        {
+            if (btnLinha1Coluna2.Text == "")
+            {
+                btnLinha1Coluna2.Text = simbolo;
+                dados[0, 1] = simbolo;
+                IdentificarVencedor();
+                MudarJogador();
+            }
+        }
+        private void btnLinha1Coluna3_Click(object sender, EventArgs e)
+        {
+            if (btnLinha1Coluna3.Text == "")
+            {
+                btnLinha1Coluna3.Text = simbolo;
+                dados[0, 2] = simbolo;
+                IdentificarVencedor();
+                MudarJogador();
+            }
+        }
+
+        private void btnLinha2Coluna1_Click(object sender, EventArgs e)
+        {
+            if (btnLinha2Coluna1.Text == "")
+            {
+                btnLinha2Coluna1.Text = simbolo;
+                dados[1, 0] = simbolo;
+                IdentificarVencedor();
+                MudarJogador();
+            }
+        }
+
+        private void btnLinha2Coluna2_Click(object sender, EventArgs e)
+        {
+            if (btnLinha2Coluna2.Text == "")
+            {
+
+                btnLinha2Coluna2.Text = simbolo;
+                dados[1, 1] = simbolo;
+                IdentificarVencedor();
+                MudarJogador();
+            }
+        }
+
+        private void btnLinha2Coluna3_Click(object sender, EventArgs e)
+        {
+            if (btnLinha2Coluna3.Text == "")
+            {
+                btnLinha2Coluna3.Text = simbolo;
+                dados[1, 2] = simbolo;
+                IdentificarVencedor();
+                MudarJogador();
+            }
+        }
+
+        private void btnLinha3Coluna1_Click(object sender, EventArgs e)
+        {
+            if (btnLinha3Coluna1.Text == "")
+            {
+                btnLinha3Coluna1.Text = simbolo;
+                dados[2, 0] = simbolo;
+                IdentificarVencedor();
+                MudarJogador();
+            }
+        }
+
+        private void btnLinha3Coluna2_Click(object sender, EventArgs e)
+        {
+            if (btnLinha3Coluna2.Text == "")
+            {
+                btnLinha3Coluna2.Text = simbolo;
+                dados[2, 1] = simbolo;
+                IdentificarVencedor();
+                MudarJogador();
+            }
+        }
+
+        private void btnLinha3Coluna3_Click(object sender, EventArgs e)
+        {
+            if (btnLinha3Coluna3.Text == "")
+            {
+                btnLinha3Coluna3.Text = simbolo;
+                dados[2, 2] = simbolo;
+                IdentificarVencedor();
+                MudarJogador();
+            }
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            dados = new string[3, 3];
+            lblJogador.Text = "Jogador 1";
+            simbolo = "X";
+
+            btnLinha1Coluna1.Text = btnLinha1Coluna2.Text = btnLinha1Coluna3.Text = "";
+            btnLinha2Coluna1.Text = btnLinha2Coluna2.Text = btnLinha2Coluna3.Text = "";
+            btnLinha3Coluna1.Text = btnLinha3Coluna2.Text = btnLinha3Coluna3.Text = "";
+        }
+
+        private bool IdentificarVencedor()
+        {
+            bool venceu;
+            //horizontal
+            if ((dados[0, 0] == dados[0, 1]) && (dados[0, 0] == dados[0, 2]) && !String.IsNullOrWhiteSpace(dados[0, 0]))
+            {
+                venceu = true;
+            }
+            else if ((dados[1, 0] == dados[1, 1]) && (dados[1, 0] == dados[1, 2]) && !String.IsNullOrWhiteSpace(dados[1, 0]))
+            {
+                venceu = true;
+            }
+            else if ((dados[2, 0] == dados[2, 1]) && (dados[2, 0] == dados[2, 2]) && !String.IsNullOrWhiteSpace(dados[2, 0]))
+            {
+                venceu = true;
+            }
+
+            //vertical
+            else if ((dados[0, 0] == dados[1, 0]) && (dados[0, 0] == dados[2, 0]) && !String.IsNullOrWhiteSpace(dados[0, 0]))
+            {
+                venceu = true;
+            }
+            else if ((dados[0, 1] == dados[1, 1]) && (dados[0, 1] == dados[2, 1]) && !String.IsNullOrWhiteSpace(dados[0, 1]))
+            {
+                venceu = true;
+            }
+            else if ((dados[0, 2] == dados[1, 2]) && (dados[0, 2] == dados[2, 2]) && !String.IsNullOrWhiteSpace(dados[0, 2]))
+            {
+                venceu = true;
+            }
+
+            //diagonal esquerda para direita
+            else if ((dados[0, 0] == dados[1, 1]) && (dados[0, 0] == dados[2, 2]) && !String.IsNullOrWhiteSpace(dados[0, 0]))
+            {
+                venceu = true;
+            }
+            //diagonal direita para esquerda
+            else if ((dados[0, 2] == dados[1, 1]) && (dados[0, 2] == dados[2, 0]) && !String.IsNullOrWhiteSpace(dados[0, 2]))
+            {
+                venceu = true;
+            }
+            else
+            {
+                venceu = false;
+            }
+
+
+            if (venceu)
+            {
+                MessageBox.Show(lblJogador.Text + " é o vencedor.");
+                btnRestart_Click(null, null);
+            }
+            else if (
+                !String.IsNullOrWhiteSpace(dados[0, 0]) &&
+                !String.IsNullOrWhiteSpace(dados[0, 1]) &&
+                !String.IsNullOrWhiteSpace(dados[0, 2]) &&
+                !String.IsNullOrWhiteSpace(dados[1, 0]) &&
+                !String.IsNullOrWhiteSpace(dados[1, 1]) &&
+                !String.IsNullOrWhiteSpace(dados[1, 2]) &&
+                !String.IsNullOrWhiteSpace(dados[2, 0]) &&
+                !String.IsNullOrWhiteSpace(dados[2, 1]) &&
+                !String.IsNullOrWhiteSpace(dados[2, 2])
+                )
+            {
+                MessageBox.Show("Deu velha!");
+            }
+            return venceu;
+        }
+
+        private void MudarJogador()
+        {
+            if (simbolo == "X")
+            {
+                simbolo = "O"; lblJogador.Text = "Jogador 2";
+            }
+            else
+            {
+                simbolo = "X"; lblJogador.Text = "Jogador 1";
+            }
+        }
+    }
+}
